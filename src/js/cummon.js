70e895b47e04eaf983ebf924f17cd4b61ce11403
balexnick <leha.alexey1909@gmail.com>
2019-11-19 18:16:28 +0200

    activ&not active pages

--- src/js/cummon.js
+++ src/js/cummon.js
@@ -1,34 +1,47 @@
 let getCode = document.querySelectorAll('.get-code')
-let text = document.querySelector(".copy-code").textContent;
+let text = document.querySelector(".copy-code");
 let button = document.querySelector(".copy-btn");
 let test = document.querySelector(".test");
-getCode.forEach((item) => {
-  item.addEventListener('click', () => {
-    document.querySelector('.popup').style.cssText = `
-      display: flex
-    `
+
+if (getCode.length !== 0) {
+  getCode.forEach((item) => {
+    item.addEventListener('click', () => {
+      document.querySelector('.popup').style.cssText = `
+        display: flex
+      `
+    })
   })
-})
 
-button.addEventListener("click", function () {
-  var x = document.createElement("INPUT");
-  x.className = "input-copy";
-  x.setAttribute("type", "text");
-  x.setAttribute("value", text);
-  document.body.appendChild(x);
-  document.querySelector("INPUT").select();
-  document.execCommand("copy");
-  this.innerHTML = "COPIED";
-  this.style.cssText = `background: #7666C7;`
-  document.querySelector('.copy-code').style.cssText = `
-    text-decoration: line-through
-  `
-  test.appendChild(x);
-  test.removeChild(x);
-});
+  button.addEventListener("click", function () {
+    var x = document.createElement("INPUT");
+    x.className = "input-copy";
+    x.setAttribute("type", "text");
+    x.setAttribute("value", text.textContent);
+    document.body.appendChild(x);
+    document.querySelector("INPUT").select();
+    document.execCommand("copy");
+    this.innerHTML = "COPIED";
+    this.style.cssText = `background: #7666C7;`
+    document.querySelector('.copy-code').style.cssText = `
+      text-decoration: line-through
+    `
+    test.appendChild(x);
+    test.removeChild(x);
+  });
 
-document.querySelector('.close-btn').addEventListener('click', () => {
+  document.querySelector('.close-btn').addEventListener('click', () => {
+    returnToDefault()
+  })
+}
+
+function returnToDefault() {
   document.querySelector('.popup').style.cssText = `
-    display: none
+  display: none
+`
+  document.querySelector(".copy-btn").innerHTML = "TAP TO COPY";
+  document.querySelector(".copy-btn").style.cssText = `background: #00D66F;`
+  document.querySelector('.copy-code').style.cssText = `
+    text-decoration: inherit;
   `
-})
+}
+
